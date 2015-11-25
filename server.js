5eef03145f7b7fbd449ab7a18cf26b35e2249993
Stally <the.stally@gmail.com>
2015-11-25 19:17:51 +0000

    Tidied up routing

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var express = require("express");
 var app = express();
-var router = express.Router();
 var port = process.env.PORT;
 
 // Templating engine
-app.set('views', __dirname + '/views');
+app.set("views", __dirname + "/views");
 app.set("view engine", "jade");
 
 // Set route for static content
@@ -12,12 +11,7 @@ app.use(express.static(__dirname + "/bower_components"));
 app.use(express.static(__dirname + "/public"));
 
 // Routes
-require('./routes/home')(app);
-
-// Default route to catch 404 errors
-app.use("*",function(req,res){
-  res.render("404");
-});
+app.use(require("./routes"));
 
 // Start server
 app.listen(port,function(){
